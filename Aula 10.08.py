faf66e023a9847bdeccd0aadee2c30ed83485c08
Pedro <pedro.ataide@aluno.fmpsc.edu.br>
2026-08-10 10:45:24 -0300

    Add files via upload

---
+++ Aula 10.08.py
@@ -0,0 +1,26 @@
+'''
+Comentário
+de
+linhas
+'''
+#Comentário de uma linha
+print('Olá Mundo!\nBem-vindos turma 2026.2!')
+print('"ola filho"')
+#Variáveis
+nome = 'Pedro Waltrick'
+idade= 22 #integer - Nº inteiro
+altura = 1.71 #float - Nº reais
+print(nome,',',idade,',',altura)
+print(nome)
+print(idade)
+print(altura)
+#tipagem dinâmica
+exemplo = 'um'
+print(type(exemplo))
+exemplo = 1
+print(type(exemplo))
+exemplo = 1.1
+print(type(exemplo))
+print(nome,'tem',idade,'anos e mede',altura,'.')
+print(nome, 'tem', idade, 'anos e mede', altura, '.')
+
